a7ee3679cbbb199a7e6fc6d36329f6a4c053ef3b
Jaque <45607757+japerez107@users.noreply.github.com>
2019-09-03 19:47:36 -0400

    readme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,22 +1,37 @@
 import React from "react";
 
+
+
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-
-      <div id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item" id="home">
-                        <a className="nav-link" href="/"><button type="button" className="btn btn-info text-white">Search Books</button></a>
-                    </li>
-                    <li className="nav-item" id="save">
-                        <a className="nav-link" href="/Saved"><button type="button" className="btn btn-info text-white">Saved Books</button></a>
-                    </li>
-                </ul>
-            </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Book It! Google Books
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/book"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/saved"
+              className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+            >
+              Saved Books
+            </Link>
+          </li>
+          </ul>
+      </div>
     </nav>
   );
 }
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const mongoose = require ("mongoose");
 const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Use apiRoutes
-app.use("/api", apiRoutes);
+app.use(apiRoutes);
 
 // Send every request to the React app
 // Define any API routes before this runs
